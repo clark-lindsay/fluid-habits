b3a0f0543abcecbba8197766e0d936bb5f5edb90
clark-lindsay <clarklindsay96@gmail.com>
2022-10-22 20:57:58 -0400

    disable credo check for mocked module

--- lib/fluid_habits/broadcasters/mock_broadcaster.ex
+++ lib/fluid_habits/broadcasters/mock_broadcaster.ex
@@ -3,6 +3,7 @@ defmodule FluidHabits.Broadcasters.MockBroadcaster do
 
   @impl FluidHabits.Broadcasters.Broadcaster
   def broadcast(_module, topic, message) do
+    # credo:disable-for-next-line
     IO.inspect(message, label: 'Broadcast for topic `#{topic}`')
   end
 end
