eaf80ad572636287788c10a08aff3707a28606fe
clark-lindsay <clarklindsay96@gmail.com>
2022-11-08 20:16:27 -0500

    Fix: handle time zones better for splitting, add tests
      - modified the function head so that both the `period_start` and
        `period_end` have the same time zone
      - modify the property testing to also vary the `time_zone`
      - the critical fix in the function necessitated by the new testing was
        to adjust the grouping (and thus the sorting) so that a "week" in
        the grouping and a "week" for the `:granularity` argument both line
        up such that they consider "Monday" the first day of the week

--- lib/fluid_habits/datetime.ex
+++ lib/fluid_habits/datetime.ex
@@ -1,32 +1,41 @@
 defmodule FluidHabits.DateTime do
   def split_into_intervals(
-        %DateTime{} = period_start,
-        %DateTime{} = period_end,
+        %DateTime{time_zone: time_zone} = period_start,
+        %DateTime{time_zone: time_zone} = period_end,
         granularity \\ :weeks
       ) do
     if Timex.after?(period_start, period_end) do
       []
     else
-      Timex.Interval.new(from: period_start, until: period_end, step: [days: 1])
+      Timex.Interval.new(
+        from: DateTime.to_naive(period_start),
+        until: DateTime.to_naive(period_end),
+        right_open: false,
+        step: [days: 1]
+      )
       |> Enum.to_list()
-      |> Enum.group_by(&Timex.iso_week/1)
-      |> Enum.sort(:asc)
-      |> Enum.map(fn {{_year, _week}, datetimes} ->
+      |> Enum.group_by(&Timex.end_of_week(&1, :mon))
+      |> Enum.sort(fn {end_of_week_a, _date_a}, {end_of_week_b, _date_b} ->
+        Timex.before?(end_of_week_a, end_of_week_b)
+      end)
+      |> Enum.map(fn {_end_of_week, date_times} ->
         from =
-          hd(datetimes)
-          |> Timex.to_datetime()
+          hd(date_times)
+          |> Timex.to_datetime(period_start.time_zone)
           |> Timex.beginning_of_day()
 
+        end_of_week = Timex.end_of_week(from, :mon)
+
         until =
-          if Timex.before?(period_end, Timex.end_of_week(from)) do
+          if Timex.before?(period_end, end_of_week) do
             period_end
           else
-            Timex.end_of_week(from)
+            end_of_week
           end
 
         %{
-          from: from,
-          until: until
+          from: DateTime.shift_zone!(from, "Etc/UTC"),
+          until: DateTime.shift_zone!(until, "Etc/UTC")
         }
       end)
     end
--- test/fluid_habits/datetime_test.exs
+++ test/fluid_habits/datetime_test.exs
@@ -18,21 +18,73 @@ defmodule FluidHabits.DateTimeTest do
       assert [] == FluidHabits.DateTime.split_into_intervals(period_start, period_end)
     end
 
+    test "start and end must have the same time_zone" do
+      now_eastern = Timex.now() |> DateTime.shift_zone!("US/Eastern")
+      tomorrow_pacific = Timex.now() |> Timex.shift(days: 1) |> DateTime.shift_zone!("US/Pacific")
+
+      assert_raise FunctionClauseError,
+                   fn ->
+                     FluidHabits.DateTime.split_into_intervals(now_eastern, tomorrow_pacific)
+                   end
+    end
+
+    test "set of intervals covers exactly the time period from start to end" do
+      # excluding "Etc" zones due to a weird issue where midnight in the Etc-01 timezone
+      # and 23:00 the day before were not comparing as equal
+      check all(
+              start_year <- StreamData.integer(2020..2025),
+              start_day <- StreamData.integer(1..365),
+              end_year <- StreamData.integer(2020..2025),
+              end_day <- StreamData.integer(1..365),
+              time_zone <-
+                StreamData.member_of(
+                  Timex.timezones()
+                  |> Enum.filter(fn tz -> !String.contains?(tz, "Etc") end)
+                )
+            ) do
+        period_start =
+          Timex.from_iso_day(start_day, start_year)
+          |> Timex.to_datetime()
+          |> DateTime.shift_zone!(time_zone)
+
+        period_end =
+          Timex.from_iso_day(end_day, end_year)
+          |> Timex.to_datetime()
+          |> DateTime.shift_zone!(time_zone)
+
+        {period_start, period_end} =
+          if Timex.before?(period_start, period_end) do
+            {period_start, period_end}
+          else
+            {period_end, period_start}
+          end
+
+        {period_start, period_end} =
+          {Timex.beginning_of_day(period_start), Timex.end_of_day(period_end)}
+
+        intervals = FluidHabits.DateTime.split_into_intervals(period_start, period_end)
+
+        assert Timex.equal?(period_start, hd(intervals)[:from])
+        assert Timex.equal?(period_end, List.last(intervals)[:until])
+      end
+    end
+
     test "set of returned intervals is strictly ordered, and each interval is strictly ordered" do
       check all(
               start_year <- StreamData.integer(2020..2025),
               start_day <- StreamData.integer(1..365),
               end_year <- StreamData.integer(2020..2025),
-              end_day <- StreamData.integer(1..365)
+              end_day <- StreamData.integer(1..365),
+              time_zone <- StreamData.member_of(Timex.timezones())
             ) do
         period_start =
           Timex.from_iso_day(start_day, start_year)
-          |> Timex.to_datetime()
+          |> Timex.to_datetime(time_zone)
           |> Timex.beginning_of_day()
 
         period_end =
           Timex.from_iso_day(end_day, end_year)
-          |> Timex.to_datetime()
+          |> Timex.to_datetime(time_zone)
           |> Timex.end_of_day()
 
         {period_start, period_end} =
